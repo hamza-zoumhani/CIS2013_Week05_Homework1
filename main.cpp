555f992188cd1ea8aa8f3a42b27f6195eb088b2d
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-26 22:00:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,29 @@
 
 using namespace std;
 
+int edoc, candy;
+int margorp=0;
+int exp=0;
+
+
 
 
 
 int main(){
 	cout << "Welcome to Edoc-Margorp game!" << endl;
 	
-	cout << 
+	cout << "How many Edoc do you have: ";
+	cin >> edoc;
+	cout << "How many Edoc candies do you have: ";
+	cin >> candy;
+	
+	do{
+		if{
+		
+		
+		
+	}
+	while(candy > 12);
 	
 	
 	return 0;
